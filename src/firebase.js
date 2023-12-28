15964441bc2728cb6fa1c114b4e6fdc739552d1f
SuneeshBalabommu <suneeshchinnu333@gmail.com>
2023-12-29 00:17:53 +0530

    all flocal files added

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDtslg55QW8XJd6e_3zmorZKhRK6Xwc8MI",
+  authDomain: "netflix-clone-6c5fb.firebaseapp.com",
+  projectId: "netflix-clone-6c5fb",
+  storageBucket: "netflix-clone-6c5fb.appspot.com",
+  messagingSenderId: "383848627744",
+  appId: "1:383848627744:web:1058e1c5a71df46503c645",
+  measurementId: "G-16XQ3MCVH3"
+};
